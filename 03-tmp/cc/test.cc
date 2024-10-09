30b80ea559d08d3073be6d806a25d0f0a4ab99ef
tcltcl <2442494081@qq.com>
2024-10-09 13:38:03 +0800

    .

--- 03-tmp/cc/test.cc
+++ 03-tmp/cc/test.cc
@@ -1,19 +1,26 @@
 #include <iostream>
-using namespace std;
+class Singleton {
+private:
+    // 私有构造函数
+    Singleton() {}
 
-int f(int a []){
-    return sizeof(a);
-}
+public:
+    // 获取实例的静态方法
+    static Singleton& getInstance() {
+        static Singleton instance; // 懒汉式，第一次调用时创建
+        return instance;
+    }
+};
 
-int main()
-{
-    int a [] = {1,2,3,4,5};
-    int s1, s2, s3;
-    s1 = sizeof(a);
-    int *p = a;
-    s2 = sizeof(p);
-    s3 = f(a);
-    printf("%d %d %d", s1, s2, s3);
+// 使用示例
+int main() {
+    Singleton& singleton1 = Singleton::getInstance();
+    Singleton& singleton2 = Singleton::getInstance();
+    Singleton s3 = singleton2;
+
+    // 打印单例对象的地址
+    std::cout << "Address of singleton1: " << &singleton1 << std::endl;
+    std::cout << "Address of singleton2: " << &singleton2 << std::endl;
+    std::cout << "Address of singleton2: " << &s3 << std::endl;
     return 0;
 }
-
--- 03-tmp/py/test.py
+++ 03-tmp/py/test.py
@@ -1,31 +1,17 @@
-import numpy as np
+def singleton(cls):   # cls是类而不是对象
+    _instance = {}
 
-def weighted_moving_average(data, window_size):
-    """
-    计算数据序列的加权移动平均值。
+    def inner():
+        if cls not in _instance:      # 没有类
+            _instance[cls] = cls()    # 则创建一个对象，{类:对象}
+        return _instance[cls]
+    return inner
     
-    参数:
-    data (list/numpy.ndarray) - 待计算移动平均的数据序列
-    window_size (int) - 移动窗口的大小
-    
-    返回:
-    numpy.ndarray - 加权移动平均值序列
-    """
-    n = len(data)
-    head = (window_size-1)//2
-    tail = head+n-window_size+1
-    weighted_avg = np.zeros(n)
-    
-    # 计算中间部分的加权平均值
-    weights = np.ones(window_size) / window_size
-    weighted_avg[head:tail] = np.convolve(data, weights, 'valid')
-    
-    # 计算两边部分的加权平均值
-    for i in range(head):
-        weighted_avg[i] = data[:1+i].mean()
-    for i in range(tail, n):
-        weighted_avg[i] = data[tail:1+i].mean()
-    return weighted_avg
+@singleton
+class Cls(object):
+    def __init__(self):
+        pass
 
-data = np.array([1,2,3,4,5])
-print(weighted_moving_average(data, 2))
+cls1 = Cls()   # singleton(Cls)(), 即inner()
+cls2 = Cls()
+print(id(cls1) == id(cls2))    # True
